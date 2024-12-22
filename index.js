1bd17937d07c3799d7ff0130a24d71ae26d90907
Saloni Sharma <140889549+salonisharma9700@users.noreply.github.com>
2024-12-22 16:22:59 +0530

    Update

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const timeroute = require('./routes/timeroute');
+
 const app = express();
 const PORT = 4000;
-const cors = require('cors');
 
-const corsOptions = {
-  origin: "http://localhost:3000"
-};
-app.use(cors(corsOptions));
-
-
-mongoose.connect('', {
+mongoose.connect('mongodb://localhost:27017/wordgame', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
-
-app.use( require('./routes/timeroute'));
-
+app.use(timeroute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
